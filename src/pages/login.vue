3ea775f2b8b2fdf6fc78059012a7c3d969ae2d43
relara <relvira_7232@stu.southtexascollege.edu>
2022-03-01 20:44:31 -0600

    register done

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -4,7 +4,7 @@ import { useRouter } from "vue-router";
 import useAuth from "../composable/useAuth";
 import useError from "../composable/useError";
 
-const { isAuthenticated, login } = useAuth();
+const { isAuthenticated, login, signup } = useAuth();
 
 const username = ref("");
 const password = ref("");
@@ -13,13 +13,23 @@ const router = useRouter();
 
 const logginIn = async () => {
     await login(username.value, password.value);
+    goToHome();
+};
+
+const signingUp = async () => {
+    await signup(username.value, password.value);
+    goToHome();
+};
+
+const goToHome = () => {
     if (isAuthenticated.value) {
-       router.push("/");
+        router.push("/");
     } else {
         setError("Invalid username or password");
         start();
-    };
+    }
 };
+
 
 const { error, setError } = useError();
 import { promiseTimeout, useTimeout } from '@vueuse/core'
@@ -33,7 +43,10 @@ const { ready, start } = useTimeout(3000, { controls: true });
        <form @submit.prevent="logginIn" class="flex flex-col p-4 space-y-4">
            <input type="text" class="p-2 border-2 rounded-lg" placeholder="Username" v-model="username" />
            <input type="password" class="p-2 border-2 rounded-lg" placeholder="Password" v-model="password" />
-           <button type="submit" @submit.prevent="logginIn" class="py-2 text-indigo-200 bg-indigo-600 rounded-lg">Login</button>
+           <div class="flex space-x-2">
+             <button type="submit" @submit.prevent="logginIn" class="w-1/2 py-2 text-yellow-200 bg-yellow-600 rounded-lg">Login</button>
+             <button @click="signingUp" class="w-1/2 py-2 text-green-200 bg-green-600 rounded-lg">SignUp</button>   
+           </div>
         </form>   
     </div>
     <div v-if="!ready && error"  class="bg-red-300 absolute bottom-2 right-2 w-1/3 rounded-lg p-4 text-center text-red-800">
