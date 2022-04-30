5146b68dc95d27814f00928dbca967239b1f7635
relara <relvira_7232@stu.southtexascollege.edu>
2022-04-29 20:32:42 -0500

    send message done

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,10 +1,17 @@
 <script setup>
 import { onUnmounted, ref } from 'vue'
 import useChat from '../composable/useChat'
+import useAuth from '../composable/useAuth'
 
-const { messages, unsubscribe } = useChat()
+const { messages, unsubscribe, sendMessage, } = useChat()
+const { user } = useAuth()
 
 const newMessage = ref('')
+
+const send = () => {
+    sendMessage(newMessage.value)
+    newMessage.value = ''
+}
 
 onUnmounted(() => {
     unsubscribe()
@@ -16,14 +23,15 @@ onUnmounted(() => {
 <div class="min-h-[500px] w-full mt-8 rounded-lg shadow-2xl flex flex-col justify-between">
     <ul class="p-4 space-y-4">
         <li v-for="message in messages" :key="message.id">
-            <div class="flex justify-between px-4 py-2 bg-gray-200 rounded-lg">
+            <div class="flex justify-between px-4 py-2 rounded-lg" :class="user===message.author ? 'bg-yellow-200' : 'bg-gray-200'">
                 <span>{{ message.text }}</span>
-                <span>by{{ message.author }}</span>
+                <span>by  {{ message.author }}</span>
             </div>
         </li>
     </ul>
     <div>
-        <input class="w-full p-4 rounded-lg focus:outline-none focus:bg-yellow-200" type="text" placeholder="Type a message..." v-model="newMessage"/>
+        <input class="w-full p-4 rounded-lg focus:outline-none focus:bg-yellow-200" type="text" placeholder="Type a message..." v-model="newMessage"
+        @change="send" />
     </div>
 </div>
 </template>
